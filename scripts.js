0200ff0ed7c4b8ca27744e58b212aa92378a324a
zach <ZCroninDev@users.noreply.github.com>
2021-06-08 07:57:49 -0400

    Animates text WIP

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('m16 .letters');
+var textWrapper = document.querySelector('.ml6 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline.timeline({loop:true})
-    .add({
-        targets: '.m16 .letter',
-        translateY: ["1.1em", 0],
-        tanslateZ: 0,
-        duration: 750,
-        delay: (el, i) => 50 * i
-    }) .add({
-        targets: '.m16', 
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-    });
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
